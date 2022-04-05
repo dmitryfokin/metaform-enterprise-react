1b6fade6871f19f2ab5c863252fbf199e51dd1df
Dmitry Fokin <dkoder.dvl@gmail.com>
2022-04-05 11:07:28 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import EnterprisePage from './pages/EnterprisePage'
+import ConfigurationPage from './pages/ConfigurationPage'
 
 function App() {
   return (
-    <div>
-      WORKING blya
-    </div>
+    <Routes>
+      {/* <Route path={'/enterprise'} element={<EnterprisePage />}/> */}
+      <Route path={'/e'} element={<EnterprisePage />}/>
+      {/* <Route path={'/configuration'} element={<ConfigurationPage />}/> */}
+      {/* <Route path={'/conf'} element={<ConfigurationPage />}/> */}
+      <Route path={'/c'} element={<ConfigurationPage />}/>
+      <Route path={'*'} element={<EnterprisePage />}/>
+    </Routes>
   );
 }
 
